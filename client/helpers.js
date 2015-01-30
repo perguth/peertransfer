7baf1f3fd789656ac092d78d447a379070a2b695
Per Guth <mail@perguth.de>
2015-01-30 14:52:22 +0100

    Clean up

--- client/helpers.js
+++ client/helpers.js
@@ -12,8 +12,8 @@ generatePassword = function() {
 }
 getAnchorAndPassword = function() {
   anchor = window.location.href.toString().split(window.location.host)[1]
-  if (anchor[0] === '/') anchor = anchor.substring(1)
-  if (anchor[0] === '#') anchor = anchor.substring(1)
+  while (anchor[0] === '/') anchor = anchor.substring(1)
+  while (anchor[0] === '#') anchor = anchor.substring(1)
   if (anchor) {
     anchor = anchor.split(':')
     password = anchor[1]
@@ -22,27 +22,25 @@ getAnchorAndPassword = function() {
 }
 binaryToBlob = function(decrypted) {
   // See http://stackoverflow.com/a/10473992
-  var raw_data = atob(decrypted.split(',')[1]);
+  var raw_data = atob(decrypted.split(',')[1])
   // Use typed arrays to convert the binary data to a Blob
-  var arraybuffer = new ArrayBuffer(raw_data.length);
-  var view = new Uint8Array(arraybuffer);
+  var arraybuffer = new ArrayBuffer(raw_data.length)
+  var view = new Uint8Array(arraybuffer)
   for (var i=0; i<raw_data.length; i++) {
-    view[i] = raw_data.charCodeAt(i) & 0xff;
+    view[i] = raw_data.charCodeAt(i) & 0xff
   }
   try {
     // This is the recommended method:
-    var blob = new Blob([arraybuffer], {type: 'application/octet-stream'});
+    var blob = new Blob([arraybuffer], {type: 'application/octet-stream'})
   } catch (e) {
     // The BlobBuilder API has been deprecated in favour of Blob, but older
     // browsers don't know about the Blob constructor
-    // IE10 also supports BlobBuilder, but since the `Blob` constructor
-    // also works, there's no need to add `MSBlobBuilder`.
-    var bb = new (window.WebKitBlobBuilder || window.MozBlobBuilder);
-    bb.append(arraybuffer);
-    var blob = bb.getBlob('application/octet-stream'); // <-- Here's the Blob
+    var bb = new (window.WebKitBlobBuilder || window.MozBlobBuilder)
+    bb.append(arraybuffer)
+    var blob = bb.getBlob('application/octet-stream') // <-- Here's the Blob
   }
   // Use the URL object to create a temporary URL
-  url = (window.webkitURL || window.URL).createObjectURL(blob);
+  url = (window.webkitURL || window.URL).createObjectURL(blob)
   return url
 }
 sendOnIncoming = function(ptr, file, password) {
@@ -50,19 +48,19 @@ sendOnIncoming = function(ptr, file, password) {
     var reader = new FileReader()
     reader.onload = function(e){
       log(e.target.result)
-      var enc = sjcl.encrypt(password, e.target.result);
+      var enc = sjcl.encrypt(password, e.target.result)
       ptr.send(enc)
     };
-    reader.readAsDataURL(file);
+    reader.readAsDataURL(file)
   })
 }
 step = function(i) {
-  if(i == 1) back.fadeOut()
+  if (i == 1) back.fadeOut()
   else back.fadeIn()
-  stage.css('top',(-(i-1)*100)+'%');
+  stage.css('top',(-(i-1)*100)+'%')
 }
 checkValidity = function(file) {
-  if(!/^data:/.test(file)){
+  if (!/^data:/.test(file)){
     return false
   } else return true
 }
